6816711d6e67007f5865ec6d6013af681c91047d
kristopherva@hotmail.com <kristopherva@hotmail.com>
2020-08-20 15:30:34 -0400

    Updates

---
+++ precache-manifest.8512e9e6e27ebfc80cecf7a28165df4a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "816df2ef33f0fdf27f441dbe3399a01d",
+    "url": "/personalpage/index.html"
+  },
+  {
+    "revision": "0b49ee8ae846fec5eef3",
+    "url": "/personalpage/static/css/2.9c140425.chunk.css"
+  },
+  {
+    "revision": "eb1ba6c23ba3618da15e",
+    "url": "/personalpage/static/css/main.1f9efd40.chunk.css"
+  },
+  {
+    "revision": "0b49ee8ae846fec5eef3",
+    "url": "/personalpage/static/js/2.6b446a59.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/personalpage/static/js/2.6b446a59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb1ba6c23ba3618da15e",
+    "url": "/personalpage/static/js/main.138fa4f5.chunk.js"
+  },
+  {
+    "revision": "f95e13642532296676f6",
+    "url": "/personalpage/static/js/runtime-main.48def7d3.js"
+  },
+  {
+    "revision": "a08f59f44e32ab5f5fe579b7b4088b97",
+    "url": "/personalpage/static/media/linkedin.a08f59f4.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/personalpage/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/personalpage/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/personalpage/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/personalpage/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "3f9415a0660cecc47e027e1fdfbc95a7",
+    "url": "/personalpage/static/media/stream.3f9415a0.jpg"
+  }
+]);
